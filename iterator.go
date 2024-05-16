aeeedae776140f8404c5a0d0d5dd9c6e89393bc8
Eugen Eisler <eugen.eisler@siemens.com>
2024-05-16 08:56:07 +0200

    feat: initial implementation, work in progress

---
+++ iterator.go
@@ -0,0 +1,39 @@
+package espocketbase
+
+import (
+	"github.com/hallgren/eventsourcing/core"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func NewIterator(aggregateType string, records []*models.Record) *Iterator {
+	return &Iterator{
+		aggregateType: aggregateType,
+		records:       records,
+		recordsCount:  len(records),
+	}
+}
+
+type Iterator struct {
+	aggregateType string
+	records       []*models.Record
+	currentIndex  int
+	recordsCount  int
+}
+
+// Next return true if there are more data
+func (i *Iterator) Next() bool {
+	i.currentIndex++
+	return i.currentIndex < i.recordsCount
+}
+
+// Value return the event
+func (i *Iterator) Value() (ret core.Event, err error) {
+	currentRecord := i.records[i.currentIndex]
+	ret = *NewEvent(currentRecord, i.aggregateType)
+	return
+}
+
+// Close closes the iterator
+func (i *Iterator) Close() {
+	i.records = nil
+}
